a62240b691f093234f007221f7792a8976bd0ec4
Crowdbotics <team@crowdbotics.com>
2024-01-26 19:10:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amgen-collaboration-45585",
+  url: "https://amgen-collaboration-45585.botics.co",
+  api: "https://amgen-collaboration-45585.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
